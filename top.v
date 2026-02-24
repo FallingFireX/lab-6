aa67590538e71a3e06b0e8d0f1635bb01f51cb16
FallingFireX <evesperson@gmail.com>
2026-02-24 16:51:32 -0700

    Update top.v

--- top.v
+++ top.v
@@ -1,12 +1,19 @@
 module top(
-  input [1:0] sw,
-  output [0] led
+  input [3:0] sw,
+  output [2:0] led
 );
 
     light light_inst(
       .upstairs(sw[0]),
       .downstairs(sw[1]),
       .stair_light(led[0])
+    );
+
+  adder adder_inst(
+      .A(sw[2]),
+      .B(sw[3]),
+      .Y(led[1]),
+      .carry(led[2])
     );
 
 endmodule
