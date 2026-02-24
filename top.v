9cbd65a634f985b18372be27130d797b03831241
FallingFireX <evesperson@gmail.com>
2026-02-24 16:44:18 -0700

    Update top.v

--- top.v
+++ top.v
@@ -1,1 +1,12 @@
-// Implement top level module
+module top(
+  input [1:0] sw,
+  output [0] led
+);
+
+    light light_inst(
+      .upstairs(sw[0]),
+      .downstairs(sw[1]),
+      .stair_light(led[0])
+    );
+
+endmodule
