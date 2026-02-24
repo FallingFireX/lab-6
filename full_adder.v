b25aa80c4b929ca5fc9bbee1eaa8b15ac9385dab
FallingFireX <evesperson@gmail.com>
2026-02-24 16:55:47 -0700

    Update full_adder.v

--- full_adder.v
+++ full_adder.v
@@ -1,1 +1,10 @@
-// Implement module called full_adder
+module full_adder(
+  input A, B, Cin,
+  output Y,
+  output carry
+);
+  
+  assign Y = ;//equation here
+  assign carry = ;//equation here
+
+endmodule
